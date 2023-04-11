9e8b5f6a9a4075bc65af6d08b585102cb9046cd5
raeleus <raymond.ray3k@gmail.com>
2023-04-10 23:02:03 -0700

    Update Lwjgl3Launcher.java

--- lwjgl3/src/main/java/com/ray3k/unbox2d/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/ray3k/unbox2d/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,6 @@ package com.ray3k.unbox2d.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.ray3k.unbox2d.SampleGame10;
 import com.ray3k.unbox2d.SampleGame11;
 
 /** Launches the desktop (LWJGL3) application. */
